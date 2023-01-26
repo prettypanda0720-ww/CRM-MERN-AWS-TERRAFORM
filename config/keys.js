53821e3dbe13fa54cbbe8f1891d0b67764e7b3f8
prettypanda0720-ww <riverstar1992@gmail.com>
2023-01-26 11:26:35 -0800

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://polardev:rRxGvoJNnQsBUlMr@cluster0.ao95hz1.mongodb.net/test?authSource=admin&replicaSet=atlas-k35e82-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+}
